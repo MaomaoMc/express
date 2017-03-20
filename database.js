1162e1111aa6f68bda1df45d34789479d693ef22
maomao <maocui@raycloud.com>
2017-03-20 16:06:39 +0800

    论坛 test

--- database.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by maomao on 17/3/16.
- */
-var mysql = require('mysql');
-var config = require('./config/config');
-
-var pool = mysql.createPool(config.mysql_dev);
-
-exports.pool = pool;
